c64502d677beec317ee24a3d2caa14e4a761d486
Bruce Becker <brucellino@gmail.com>
2016-06-01 17:12:53 +0200

    using https, so --no-check-cert is added to the download part

--- build.sh
+++ build.sh
@@ -26,7 +26,7 @@ mkdir -p ${SOFT_DIR}
 if [ ! -e ${SRC_DIR}/${SOURCE_FILE}.lock ] && [ ! -s ${SRC_DIR}/${SOURCE_FILE} ] ; then
   touch  ${SRC_DIR}/${SOURCE_FILE}.lock
   echo "seems like this is the first build - let's geet the source"
-  wget https://root.cern.ch/download/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
+  wget --no-checl-certificate https://root.cern.ch/download/${SOURCE_FILE} -O ${SRC_DIR}/${SOURCE_FILE}
   echo "releasing lock"
   rm -v ${SRC_DIR}/${SOURCE_FILE}.lock
 elif [ -e ${SRC_DIR}/${SOURCE_FILE}.lock ] ; then
